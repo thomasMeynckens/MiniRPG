a59ab37e2e6fc21fd28e1c9aef056a4d76e4a7f4
Thomas Meynckens <meync.thom@gmail.com>
2026-06-26 16:15:27 +0200

    initialisation sdl et test fenetre 3 secondes

---
+++ build/CMakeFiles/MiniRPG.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MiniRPG.dir/src/main.cpp.obj"
+  "CMakeFiles/MiniRPG.dir/src/main.cpp.obj.d"
+  "MiniRPG.exe"
+  "MiniRPG.exe.manifest"
+  "MiniRPG.lib"
+  "MiniRPG.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MiniRPG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
